7e3e2e1c11e9e03cb177508d15c16338db8460a6
fadhlu <fadhlurahman.i@gmail.com>
2023-05-28 23:30:07 +0800

    feat: Init

---
+++ app/(site)/page.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+    return (
+      <div className="
+      flex
+      min-h-full
+      flex-col
+      justify-center
+      py-12
+      sm:px-6
+      lg:px8
+      bg-gray-100
+      ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900">
+                Sign In To <span className="
+                bg-pink-
+                px-2
+                py-2
+                rounded-md
+                text-gray-100
+                ">tanyain-aja</span>
+            </h2>
+        </div>
+      </div>
+    )
+  }
